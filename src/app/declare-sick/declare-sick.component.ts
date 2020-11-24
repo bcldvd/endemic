336ea09367aa63b7531cc3d6b2ffea1a965d7cda
pingyuan <ping.yuan@finastra.com>
2020-11-24 10:39:58 +0100

    declare a risk

--- src/app/declare-sick/declare-sick.component.ts
+++ src/app/declare-sick/declare-sick.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-declare-sick',
   templateUrl: './declare-sick.component.html',
-  styleUrls: ['./declare-sick.component.scss']
+  styleUrls: ['./declare-sick.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'app-declare-sick',
+  },
 })
 export class DeclareSickComponent implements OnInit {
+  checked: boolean;
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/declare-sick/declare-sick.module.ts
+++ src/app/declare-sick/declare-sick.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { DeclareSickRoutingModule } from './declare-sick-routing.module';
 import { DeclareSickComponent } from './declare-sick.component';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [DeclareSickComponent],
   imports: [
     CommonModule,
-    DeclareSickRoutingModule
-  ]
+    DeclareSickRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
 })
-export class DeclareSickModule { }
+export class DeclareSickModule {}
